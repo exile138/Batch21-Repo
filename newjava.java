43fc7edecb06084786f4b9db813b59109eaffe58
Ruslan Kovalchuk <rukovalchuk2@gmail.com>
2020-11-09 15:51:36 -0500

    updates 1.1

---
+++ newjava.java
@@ -0,0 +1,14 @@
+//as part of the task im adding new information!
+
+public class newJava{
+
+public static void main(String[] args) {
+
+
+String xz = "New file ";
+System.out.print(xz);
+  
+}
+
+
+}
